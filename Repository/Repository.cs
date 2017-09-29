3b0fc7e7b0065a5b4339793bb3a04ee9a4522dc1
mishmoshlife <mwandrei@inthree.com>
2017-09-28 20:57:12 -0400

    Add files via upload

---
+++ Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CoffeeNow.DAL
+{
+    public class Repository
+    {
+        private string connectionString;
+
+        public Repository()
+        {
+            connectionString = @"Data Source=logolsdb.database.windows.net;Initial Catalog=LogolsLearning;Persist Security Info=True;User ID=Team2;Password=loglolsteam#2";
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+    }
+}
