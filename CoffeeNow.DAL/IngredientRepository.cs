a68362872441fa1702886f6209a9092f0da0d5c9
mishmoshlife <mwandrei@inthree.com>
2017-09-30 22:29:52 -0400

    Add files via upload

---
+++ CoffeeNow.DAL/IngredientRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using CoffeeNow.Entities;
+using Dapper;
+using System;
+
+namespace CoffeeNow.DAL
+{
+    public class IngredientRepository : Repository
+    {
+        public int Insert(Ingredient ingredient)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+
+                 var t = dbConnection.Query<InsertID>("Team2.AddIngredient", ingredient, commandType: CommandType.StoredProcedure).First().ID;
+                Console.Write(t);
+                return t;
+            }
+        }
+
+        public void Delete(Ingredient ingredient)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute("Team2.DeleteIngredient", ingredient, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void Update(Ingredient ingredient)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute("Team2.UpdateIngredient", ingredient, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public Ingredient Get(int ingredientID)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Ingredient>("Team2.GetIngredient", new { ingredient_id = ingredientID }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+        }
+
+        public IEnumerable<Ingredient> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<Ingredient>("Team2.GetAllIngredients", commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ CoffeeNow.DAL/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using CoffeeNow.Entities;
+using Dapper;
+
+namespace CoffeeNow.DAL
+{
+    public class UserRepository : Repository
+    {
+        public int Insert(User user)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<InsertID>("Team2.AddUser", user, commandType: CommandType.StoredProcedure).First().ID;
+            }
+        }
+
+        public void Delete(User user)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute("Team2.DeleteUser", user, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute("Team2.UpdateUser", user, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public User Get(int userID)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<User>("Team2.GetUser", new { user_id = userID }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                return dbConnection.Query<User>("Team2.GetAllUsers", commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
